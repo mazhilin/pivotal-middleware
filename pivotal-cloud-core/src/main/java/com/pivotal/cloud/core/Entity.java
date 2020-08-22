83f22e069534017c0d708988f9b9dc426e9cac08
marklin <marklin1992@outlook.com>
2020-08-22 22:15:42 +0800

    develop[2020-08-22 星期六 22:14]:commit to develop

---
+++ pivotal-cloud-core/src/main/java/com/pivotal/cloud/core/Entity.java
@@ -0,0 +1,15 @@
+package com.pivotal.cloud.core;
+
+import java.io.Serializable;
+
+/**
+ * 封装Qicloud项目Entity类.<br>
+ *
+ * <p>//TODO...<br>
+ *
+ * @author Powered by marklin 2020-08-14 09:09
+ * @version 1.0.0
+ *     <p>Copyright © 2018-2020 Pivotal Cloud Technology Systems Incorporated. All rights reserved.
+ *     <br>
+ */
+public interface Entity extends Serializable {}
